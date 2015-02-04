f72f4f02ddce918e3d753608a5eb12ec084a2e4a
David Rodenas <david.rodenas@gmail.com>
2015-02-04 13:08:59 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
-# drpx-toggle
+drpx-toggle
+===========
+
 Simple Angular component that provides from a toggle that can be used as model, controller or directive
+
+
+Install
+-------
+
+```bash
+$ bower install --save drpx-toggle
+```
+
+add to your module the dependence:
+
+```javascript
+    angular.module('yourModule', ['drpxToggle']);
+```
+
+include the javascript library in your application:
+
+```html
+<script src="bower_components/drpx-toggle/drpx-toggle.js"></script>
+```
+
+
+How to use
+----------
+
+As a model:
+
+```javscript
+    YourController.$inject = ['DrpxToggle'];
+    function YourController  ( DrpxToggle ) {
+        this.toggle = new DrpxToggle();
+        // this.toggle.open()
+        // this.toggle.close()
+        // this.toggle.isOpen(): bool
+        // this.toggle.toggle()
+    }
+```
+
+As instance from the factory (like ng $cacheFactory):
+
+```javscript
+    YourController.$inject = ['drpxToggleFactory'];
+    function YourController  ( drpxToggleFactory ) {
+        this.toggle = drpxToggleFactory('yourToggleId');
+        // this.toggle.open()
+        // this.toggle.close()
+        // this.toggle.isOpen(): bool
+        // this.toggle.toggle()
+    }
+```
+
+As a controller:
+
+```html
+    <div ng-controller="DrpxToggleController as burger">
+        <span ng-show="burger.isOpen()">Is open</span>
+        <button ng-click="burger.open()">open()</button>
+        <button ng-click="burger.close()">close()</button>
+        <button ng-click="burger.toggle()">toggle()</button>
+    </div>
+```
+
+As a directive:
+
+```html
+    <div drpx-toggle="yourToggleId">
+        <span ng-show="yourToggleId.isOpen()">Is open</span>
+        <button ng-click="yourToggleId.open()">open()</button>
+        <button ng-click="yourToggleId.close()">close()</button>
+        <button ng-click="yourToggleId.toggle()">toggle()</button>
+    </div>
+```
+
+Or as directive as:
+
+```html
+    <div drpx-toggle="yourToggleId as burger">
+        <span ng-show="burger.isOpen()">Is open</span>
+        <button ng-click="burger.open()">open()</button>
+        <button ng-click="burger.close()">close()</button>
+        <button ng-click="burger.toggle()">toggle()</button>
+    </div>
+```
---
+++ drpx-toggle.js
@@ -0,0 +1,120 @@
+/*
+	DrpxToggle:
+		.isOpen()
+		.toggle()
+		.open()
+		.close()
+
+	drpxToggleFactory(name): DrpxToggle # service for given name with .destroy() to remove
+
+	DrpxToggleController: delegates to a DrpxToggle
+		.toggle
+
+	<ANY drpx-toggle="toggle [as scopeName]">...</ANY>
+		controller: DrpxToggleController
+*/
+(function(angular) {
+	'use strict';
+
+	angular
+		.module('drpxToggle', [])
+		.factory('DrpxToggle', DrpxToggleFactory)
+		.factory('drpxToggleFactory', drpxToggleFactoryFactory)
+		.controller('DrpxToggleController', DrpxToggleController)
+		.directive('drpxToggle', drpxToggleDirective)
+		;
+
+	function DrpxToggleFactory() {
+		/*jshint validthis:true */
+		function DrpxToggle() {
+			this.opened = false;
+		}
+		DrpxToggle.prototype.close = close;
+		DrpxToggle.prototype.isOpen = isOpen;
+		DrpxToggle.prototype.open = open;
+		DrpxToggle.prototype.toggle = toggle;
+
+		function close() { // jshint ignore:line
+			this.opened = false;
+		}
+		function isOpen() {
+			return this.opened;
+		}
+		function open() { // jshint ignore:line
+			this.opened = true;
+		}
+		function toggle() {
+			this.opened = !this.opened;
+		}
+
+		return DrpxToggle;
+	}
+
+	drpxToggleFactoryFactory.$inject = ['DrpxToggle'];
+	function drpxToggleFactoryFactory  ( DrpxToggle ) {
+		function drpxToggleFactory(name) {
+			var toggle;
+
+			toggle = find(name);
+			if (!toggle) {
+				toggle = create(name);
+			}
+
+			return toggle;
+		}
+
+		var toggles = {};
+
+		function create(name) {
+			var toggle;
+
+			toggle = new DrpxToggle();
+			toggle.destroy = destroy.bind(null, name);
+			toggles[name] = toggle;
+
+			return toggle;
+		}
+
+		function destroy(name) {
+			delete toggles[name];
+		}
+
+		function find(name) {
+			return toggles[name];
+		}
+
+		return drpxToggleFactory;
+	}
+
+	DrpxToggleController.$inject = ['DrpxToggle'];
+	function DrpxToggleController  ( DrpxToggle ) {
+		this.toggle = new DrpxToggle();
+	}
+
+	drpxToggleDirective.$inject = ['drpxToggleFactory'];
+	function drpxToggleDirective  ( drpxToggleFactory ) {
+		var directive = {
+			restrict: 'A',
+			controller: 'DrpxToggleController',
+			link: link,
+		};
+
+		var expr = /^\s*([A-Za-z]\w*)(\s+as\s+([A-Za-z]\w*))?\s*$/;
+
+		function link(scope, element, attrs, controller) {
+			var as, match, name, toggle;
+
+			match = attrs.drpxToggle.match(expr);
+			if (match) {
+				name = match[1];
+				as = match[3];
+				toggle = drpxToggleFactory(name);
+				if (as) { scope[as] = toggle; }
+				controller.toggle = toggle;
+			}
+		}
+
+		return directive;
+	}
+
+})(angular);
